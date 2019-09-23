0001b7e25965a45b73fe6a4f78133bc7653dd6c8
Jonathan Francisco <jonathanfrancisco999@gmail.com>
2019-09-23 22:37:11 +0800

    implement edit action

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -18,9 +18,8 @@ threadController.renderCreatePage = (req, res) => {
 
 threadController.create = async (req, res, next) => {
   try {
-    const { _id: userId } = req.user
     const { title } = req.body
-    await Thread.create({ title, user: userId })
+    await Thread.create({ title, user: req.user._id })
     return res.redirect('/threadlist')
   } catch (err) {
     next(err)
@@ -46,6 +45,31 @@ threadController.delete = async (req, res, next) => {
       await Thread.findByIdAndDelete(id)
     }
     return res.redirect('/threadlist')
+  } catch (err) {
+    next(err)
+  }
+}
+
+threadController.renderEditPage = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const thread = await Thread.findById(id)
+    return res.render('editThread', { thread })
+  } catch (err) {
+    next(err)
+  }
+  res.render('editThread')
+}
+
+threadController.edit = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { title } = req.body
+    const thread = await Thread.findById(id).populate('user')
+    if (thread.user.equals(req.user._id)) {
+      await Thread.findByIdAndUpdate(id, { title })
+    }
+    return res.redirect(`/view/${id}`)
   } catch (err) {
     next(err)
   }
--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,11 @@ indexRouter.get('/create', authenticated, threadController.renderCreatePage)
 indexRouter.post('/threadlist', authenticated, threadController.create)
 indexRouter.get('/view/:id', authenticated, threadController.get)
 indexRouter.get('/view/:id/delete', authenticated, threadController.delete)
+indexRouter.get(
+  '/view/:id/edit',
+  authenticated,
+  threadController.renderEditPage
+)
+indexRouter.post('/view/:id/edit', authenticated, threadController.edit)
 
 module.exports = indexRouter

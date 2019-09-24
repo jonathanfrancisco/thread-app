bdae6d29b65d574d4a9adf2b66315e4626e2f164
Jonathan Francisco <jonathanfrancisco999@gmail.com>
2019-09-24 08:06:37 +0800

    add comments reference to thread

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const commentSchema = new Schema(
+  {
+    body: {
+      type: String,
+      required: true
+    },
+    thread: { type: Schema.Types.ObjectId, ref: 'Thread', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: false
+    }
+  }
+)
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment

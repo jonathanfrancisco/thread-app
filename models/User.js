dd9bbdecf6e04190a031781904a6d30a176f78c2
Jonathan Francisco <jonathanfrancisco999@gmail.com>
2019-09-24 08:04:26 +0800

    add reference to threads and comments

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 const passportLocalMongoose = require('passport-local-mongoose')
-const Thread = require('../models/Thread')
 
 const { Schema } = mongoose
 
-const userSchema = new Schema({})
+const userSchema = new Schema({
+  threads: [{ type: Schema.Types.ObjectId, ref: 'Thread' }],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+})
 userSchema.plugin(passportLocalMongoose)
 
 const User = mongoose.model('User', userSchema)
